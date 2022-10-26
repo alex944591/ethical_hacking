02fec80d479900c1afdb6926b5c4a364ecce5533
alex944591 <alex944591@gmail.com>
2022-10-26 18:05:32 +0300

    working version

--- arpDetector.py
+++ arpDetector.py
@@ -5,10 +5,11 @@ def processPacket(packet):
     src_IP = packet['ARP'].psrc
     src_MAC = packet['Ether'].src
     if src_IP in IP_MAC_MAP.values():
-        if src_MAC not in IP_MAC_MAP.keys():
+        if IP_MAC_MAP[src_MAC] != src_IP:
             message = ("Possible ARP attack detected!\n"
             + "It's possible that the machine with MAC address: " + str(src_MAC)
             + " is pretending to be " + str(src_IP))
+            print(message)
     else:
         IP_MAC_MAP[src_MAC] = src_IP
 
