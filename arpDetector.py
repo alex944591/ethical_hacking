24502a093892909f116f91d030b154ca40f2a9f9
alex944591 <alex944591@gmail.com>
2022-10-26 14:21:01 +0300

    arp_detector_candidate

--- arpDetector.py
+++ arpDetector.py
@@ -1,4 +1,19 @@
 # -*- coding: utf-8 -*-
-nat = "ip nat inside source list ACL interface FastEthernet0/1 overload"
-new_line=nat.replace('Fast', 'Gigabit')
-print(new_line)
+from scapy.all import sniff
+
+
+def processPacket(packet):
+    src_IP = packet['ARP'].psrc
+    src_MAC = packet['Ether'].src
+    if src_IP in IP_MAC_MAP.values():
+        if src_MAC not in IP_MAC_MAP.keys():
+            message = ("Possible ARP attack detected!\n"
+            + "It's possible that the machine with MAC address: " + str(src_MAC)
+            + " is pretending to be " + str(src_IP))
+    else:
+        IP_MAC_MAP[src_MAC] = src_IP
+
+
+if __name__ == "__main__":
+    IP_MAC_MAP = {}
+    sniff(count=0, filter='arp', store=0, prn=processPacket)
